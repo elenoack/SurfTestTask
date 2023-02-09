5dc040f1f1d663f4d116f832279ebe71284ad020
elenoack <elenanoack@mail.ru>
2023-02-09 20:43:48 +0300

    feat: Add DTO.

---
+++ SurfTestTask/Sources/AppLayers/Module/DTO/SurfDTO.swift
@@ -0,0 +1,22 @@
+//
+//  SurfDTO.swift
+//  SurfTestTask
+//
+//  Created by Elena Noack on 09.02.23.
+//
+
+import Foundation
+
+enum SurfDTO {
+
+    enum GetContent {
+
+        struct Request {}
+        
+        struct Response {
+            let result: [ContentModel]
+        }
+        struct ViewModel {}
+    }
+
+}
